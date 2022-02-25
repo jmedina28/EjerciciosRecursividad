75b6c2b2f9c6b29fcd8278ad722007f73516ba10
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-02-25 08:57:45 +0000

    palíndromo con letras acabado

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,13 +1,14 @@
 # Palíndromos
 def palindromo(contenido):
   contenido = contenido.lower()
+  contenido = contenido.replace(' ', '')
   lista = list(contenido)
   listaresultado = list(reversed(contenido))
   if lista == listaresultado:
     print(lista, listaresultado)
-    print("Esta palabra es un palíndromo.")
+    print("Esta frase/palabra es un palíndromo.")
   else:
     print(lista, listaresultado)
     print("No es palíndromo.")
-contenido = str(input("Introduzca una palabra: "))
+contenido = str(input("Introduzca una frase/palabra: "))
 palindromo(contenido)
