0c0ccce692b11ef61f90ffed487cc8ffaba8e60c
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-02-25 10:28:51 +0000

    ejercicio 1 intentado

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+lista = ["perro","gato","hamster","koala","tigre","ciervo"]
+print(lista)
+def dicotomia(palabra, longitud):
+  if palabra == lista[round(longitud/2)]:
+    print("La palabra es " + str(lista[round(longitud/2)]))
+    if longitud >= 0:
+      print("Se encuentra en el punto " + str(round(longitud/2)))
+    else:
+      print("Se encuentra en el punto " + str(round(longitud/2)+len(lista)))
+  else:
+    dicotomia(palabra, longitud-1)
+dicotomia(str(input("Introduzca una palabra que desee localizar de la lista dada: ")), len(lista))
