3c98838e211eded201c3d15728d14fd8fca6ee1f
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-02-25 09:41:33 +0000

    readme inicial y comentarios varios

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# EjerciciosRecursividad
+<h1 align="center">Recursividad</h1>
+
+<h3 align="center">Perfil de GitHub del autor de este proyecto:</h3>
+
+1. [@jmedina28](https://github.com/jmedina28)
+
+---
+En este [repositorio](https://github.com/jmedina28/EjerciciosRecursividad) quedan resueltos los primeros ejercicios de introducción a la algorítmica. Para llevar a cabo el proyecto me he documentado a través de los ejemplos dados y la teoría que se encuentra en el campus virtual.
--- ejercicio2.py
+++ ejercicio2.py
@@ -2,12 +2,12 @@
 def palindromo(contenido):
   a,b = 'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN'
   tilde = str.maketrans(a,b)
-  contenido = contenido.lower()
-  contenido = contenido.replace(' ', '')
-  contenido = contenido.translate(tilde)
-  lista = list(contenido)
-  listaresultado = list(reversed(contenido))
-  if lista == listaresultado:
+  contenido = contenido.lower() # Convierto el texto en minúsculas.
+  contenido = contenido.replace(' ', '') # Quito los espacios.
+  contenido = contenido.translate(tilde) # Elimino las tildes.
+  lista = list(contenido) # Convierto en el contenido en una lista.
+  listaresultado = list(reversed(contenido)) # Invierto la lista.
+  if lista == listaresultado: # Comparo el contenido original con el inverso.
     print("Esta frase/palabra/número es un palíndromo.")
   else:
     print("No es palíndromo.")
