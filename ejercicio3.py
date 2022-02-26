8860ce348a4e377fc16d015b3fddba2ebe2eec00
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-02-26 17:00:37 +0000

    ejercicio final

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,28 +1,15 @@
 # Bandera:
-bandera = ["R","B","G","G","G","B","R","R","B","B","R","G","R","R","B"]
+bandera, red, green, blue = ["R","B","G","G","G","B","R","R","B","B","R","G","R","R","B"], [], [], []   # Creo las listas que posteriormente fusionaré en una.
 print("A continuación se va a ordenar la siguiente bandera: " + str(bandera))
-red, green, blue  = [], [], []
-
 def ordenrgb(bandera, posicion):
-  # Creo las listas que posteriormente fusionaré en una.
-
-  if bandera[posicion] == "R" and posicion+1 < len(bandera):
-    red.append(bandera.pop(posicion))
-    print(red)
-    ordenrgb(bandera, posicion)
-  elif bandera[posicion] == "B" and posicion+1 < len(bandera):
-    blue.append(bandera.pop(posicion))
-    print(blue)
-    ordenrgb(bandera, posicion)
-  elif bandera[posicion] == "G" and posicion+1 < len(bandera):
-    green.append(bandera.pop(posicion))
-    print(green)
+  if posicion < len(bandera):
+    if bandera[posicion] == "R":
+      red.append(bandera.pop(posicion))
+    elif bandera[posicion] == "B":
+      blue.append(bandera.pop(posicion))
+    elif bandera[posicion] == "G":
+      green.append(bandera.pop(posicion))
     ordenrgb(bandera, posicion)
   else:
-    total = red + green + blue
-    print(total)
-  
+    print("La bandera ordenada queda tal que así: "+ str(red + green + blue))
 ordenrgb(bandera, 0)
-  
-  
-  
