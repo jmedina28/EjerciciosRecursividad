db8c3a139230bf8c62e6a5fe1c5a2bf11e94e964
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-02-26 16:21:42 +0000

    continuo probando

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,3 +1,21 @@
-# Bandera
-bandera = ["R", "B","G","G","G","B","R","R","B","B","R","G","R","R","B"]
-print(bandera)
+# Bandera:
+bandera = ["R","B","G","G","G","B","R","R","B","B","R","G","R","R","B"]
+print("A continuación se va a ordenar la siguiente bandera: " + str(bandera))
+def ordenrgb(bandera, posicion):
+  # Creo las listas que posteriormente fusionaré en una.
+  red = []
+  green = []
+  blue = []
+  total = [red,green,blue]
+  if bandera[posicion] == "R":
+    red.append("R")
+    ordenrgb(bandera, posicion+1)
+
+  print(total)
+  
+  
+  
+ordenrgb(bandera, 0)
+  
+  
+  
